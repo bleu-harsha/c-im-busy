eca1365a481cfc4f71915bf669fa75b0d743f944
bleu <harshakklogu4@gmail.com>
2025-06-15 00:39:14 +0530

    init commit to scene-putting

---
+++ main.c
@@ -0,0 +1,7 @@
+//welcome to c-im-busy code, this will be the most "i-have-no-idea-what-i'm-doing-and-i-do" code
+#import <stdio.h>;
+int main(){
+  printf("i am cooked");
+  return 0;
+//this was the first commit t=regarding the main code, the project is let's say work-in-progress-and-i-have-school
+}
