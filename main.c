833067ee0e95527445b941467c81b09a9e1bd1c2
bleu <harshakklogu4@gmail.com>
2025-06-16 18:57:42 +0530

    update help

--- main.c
+++ main.c
@@ -4,7 +4,49 @@
 #include <string.h>
 #include <unistd.h>
 
-int main() {
+
+ int main(){
+  char command[64];
+  scanf("%s",command);
+  if (strcmp(command, "help") == 0 || strcmp(command, "-h") == 0 || strcmp(command,"\n") == 0)
+    printhelp(0);
+  else if{
+    printf("Unkown command: %s\n", command);
+  }
+
+  int printhelp(int reason){
+    //reason 0 displays the def command's help
+    //reason 1 displays cmatrix command's help
+    //reason 2 displays cdonut command's help
+    //reason 3 displays pattern-cool command's help
+    //reason 4 displays random command's help
+    //reason 5 displays ascii-anim command's help
+    if(reason == 0){
+    //------------------------------------------------------------banner--------------------------------------------------------------------------------
+    printf("\033[1;32m"); // Green
+    printf(" _________     .__ /\\          ___.                           \n");
+    printf(" \\_   ___ \\    |__|\\(_____     \\_ |__   __ __  ______ ___.__. \n");
+    printf(" /    \\  \\/    |  | /     \\     | __ \\ |  |  \\/  ___/<   |  | \n");
+    printf(" \\     \\____   |  ||  Y Y  \\    | \\_\\ \\|  |  /\\___ \\  \\___  | \n");
+    printf("  \\______  /   |__||__|_|  /    |___  /|____//____  > / ____| \n");
+    printf("         \\/              \\/         \\/            \\/  \\/     \n");
+    printf("\033[0m");//reset color
+    //------------------------------------------------------available commands--------------------------------------------------------------------------
+    printf("\033[1;33m");
+    printf("Available commands:");
+    printf("\033[0m");
+    printf("• help             displays this message and exits");
+    printf("• cmatrix          displays the classic cmatrix");
+    printf("• cdonut           displays the revolving donut which can be further tweaked with flags");
+    printf("• pattern-cool      displays wierd ascii patterns ");
+    printf("• random            chooses a random command and executes");
+    printf("• ascii-anim        displays ascii animations(random by default) from https://ascii.co.uk/animated");
+    printf("\033[1;33m");
+    printf("for more information on the listed commands use  cimbusy <command> help ");
+    printf("\033[0m");
+}
+
+int cdonut() {
     float A = 0, B = 0;
     float i, j;
     int k;
